6b4b8ceaf0aca2ee7a9d74f55c5cfa0eb31c8845
Rafael Almeida <rafaelpjmp@gmail.com>
2026-09-12 11:39:51 -0300

    fix: mapeamento correto dos IDs de esquemas taticos da API oficial do Cartola

--- cartola_bot/api.py
+++ cartola_bot/api.py
@@ -139,11 +139,13 @@ class CartolaAPI:
         esquema_map = {
             "3-4-3": 1,
             "3-5-2": 2,
-            "4-4-2": 3,
-            "4-3-3": 4,
-            "5-3-2": 5
+            "4-3-3": 3,
+            "4-4-2": 4,
+            "4-5-1": 5,
+            "5-3-2": 6,
+            "5-4-1": 7
         }
-        esquema_id = esquema_map.get(esquema_name, 4)
+        esquema_id = esquema_map.get(esquema_name, 3)
         
         pos_id_map = {
             'Goleiro': 1,
